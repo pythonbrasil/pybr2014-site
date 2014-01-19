cc710b773bd025acd0223eef1209cbbcbc5496c8
Fernando Rocha <fernandogrd@gmail.com>
2014-01-19 20:55:41 -0300

    S3 config
    Signed-off-by: Fernando Rocha <fernandogrd@gmail.com>

--- pythonbrasil8/settings.py
+++ pythonbrasil8/settings.py
@@ -169,3 +169,14 @@ PAGSEGURO_WEBCHECKOUT = 'https://pagseguro.uol.com.br/v2/checkout/payment.html?c
 
 COMPRESS_OFFLINE = False
 COMPRESS_ENABLED = False
+
+if os.environ.get('S3'):
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+    DEFAULT_FILE_STORAGE = 'pythonbrasil8.s3utils.MediaRootS3BotoStorage'
+    STATICFILES_STORAGE = 'pythonbrasil8.s3utils.StaticRootS3BotoStorage'
+
+    MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
