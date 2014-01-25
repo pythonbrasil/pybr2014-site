9bf9e740b524de56a1e7098d87d0ca895eb74fe0
Fernando Rocha <fernandogrd@gmail.com>
2014-01-25 15:46:40 -0300

    Cached S3 storage
    Signed-off-by: Fernando Rocha <fernandogrd@gmail.com>

--- pythonbrasil8/s3utils.py
+++ pythonbrasil8/s3utils.py
@@ -1,5 +1,10 @@
 from storages.backends.s3boto import S3BotoStorage
 from django.utils.functional import SimpleLazyObject
+from django.contrib.staticfiles.storage import CachedFilesMixin
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+
+class CachedS3BotoStorage(CachedFilesMixin, S3BotoStorage):
+    pass
+
+StaticRootS3BotoStorage = lambda: CachedS3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
