1012adfce2a32c4e666969cab1e1c9cb2f60c1ad
Fernando Rocha <fernandogrd@gmail.com>
2014-06-30 23:49:57 -0300

    Move promo code email to template
    Signed-off-by: Fernando Rocha <fernandogrd@gmail.com>

--- pythonbrasil8/promo_code/models.py
+++ pythonbrasil8/promo_code/models.py
@@ -5,6 +5,8 @@ from django.core.mail import send_mail
 from django.utils.timezone import now
 from django.conf import settings
 from django.utils.translation import ugettext as _
+from django.template.loader import get_template
+from django.template import Context
 
 
 class PromoCode(models.Model):
@@ -18,19 +20,10 @@ class PromoCode(models.Model):
         return u'{email}: {code}'.format(code=self.code, email=self.email)
 
     def send_email(self):
+        ctx = Context({'code': self.code})
+        body = get_template('promo_code/email_promo_code.txt').render(ctx)
         subject = _(u'Get your PythonBrasil[10] promotional code')
-        body = _('''
-        Hi,
-        Congratulations, here is your discount code for being a associated
-        of PythonBrasil!
 
-        Code: {}
-
-        You can complete your registration going to: <link>.
-
-        Att.
-            PythonBrasil[10] Team
-        ''').format(self.code)
         sent = send_mail(
             subject=subject, message=body, recipient_list=['to@example.com'],
             from_email=settings.DEFAULT_FROM_EMAIL,
