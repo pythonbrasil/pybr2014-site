3cd1be888a4d68ad26724c9e6e79f565962b93f7
Renato Oliveira <renatooliveira.cin@gmail.com>
2014-08-16 00:27:38 -0300

    Closing call for papers.

--- pythonbrasil8/dashboard/urls.py
+++ pythonbrasil8/dashboard/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^subscription/tutorials/$', TutorialSubscriptionView.as_view(), name='tutorials-subscription'),
     url(r'^profile/$', ProfileView.as_view(), name='edit-profile'),
     url(r'^proposals/$', SessionsView.as_view(), name='dashboard-sessions'),
-    url(r'^proposals/propose/$', SubscribeView.as_view(), name='session-subscribe'),
+    # url(r'^proposals/propose/$', SubscribeView.as_view(), name='session-subscribe'),
     url(r'^proposals/edit/(?P<id>\d+)', EditSessionView.as_view(), name='session-edit'),
     url(r'^proposals/delete/(?P<id>\d+)', DeleteSessionView.as_view(), name='session-delete'),
 )
